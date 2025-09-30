5084e11c5e521b135440cde1929df11094da4b98
Shayoo-Projects <232187964+Shayoo-Projects@users.noreply.github.com>
2025-09-30 05:43:36 -0400

    update not found page

--- index.tsx
+++ index.tsx
@@ -8,24 +8,9 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-// Support for react-snap prerendering
-const renderMethod = rootElement.hasChildNodes() ? ReactDOM.hydrateRoot : ReactDOM.createRoot;
-
-// For react-snap
-if (typeof window !== 'undefined' && 'ReactSnapClient' in window) {
-  // Use hydrate for react-snap
-  ReactDOM.hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  // Use normal rendering for development
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
